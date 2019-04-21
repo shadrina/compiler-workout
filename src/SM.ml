bfccc530aa4d67ae15578f83fe98f926f8821783
shadrina <a.shadrina5@mail.ru>
2019-04-22 03:37:44 +0700

    One-branch-case works

--- src/SM.ml
+++ src/SM.ml
@@ -49,13 +49,14 @@ let split n l =
   in
   unzip ([], l) n
           
-  let rec eval env (controlSt, st, ((s, i, o) as config)) p =
+let rec eval env (controlSt, st, ((s, i, o) as config)) p =
   let instrEval instr = match instr with
     | BINOP op -> (match st with
       | (y::x::xs) -> (Value.of_int (MyUtils.toFunc op (Value.to_int x) (Value.to_int y))::xs, config) 
       | _ -> failwith "Not enough elements in stack")
     | CONST n  -> ((Value.of_int n)::st, config)
     | STRING str -> (Value.of_string str::st, config)
+    | SEXP (t, n)-> let vs, st' = split n st in ((Value.sexp t @@ (List.rev vs))::st', config)
     | LD x       -> ((State.eval s x)::st, config)
     | ST x       -> (match st with
                       | z::tail -> (tail, ((State.update x z s), i, o))
@@ -67,17 +68,28 @@ let split n l =
                                    let fState', st' = List.fold_left (fun (s, x::st) name -> (State.update name x s, st)) (fState, st) params in
                                    (st', (fState', i, o))
     | LABEL _                   -> (st, config)
+    | DROP                      -> (List.tl st, config)
+    | DUP                       -> (List.hd st :: st, config)
+    | SWAP                      -> let x::y::st' = st in (y::x::st', config)
+    | TAG t                     -> let x::st' = st in
+                                   let res = match x with
+                                     | Value.Sexp (t', _) -> MyUtils.bti (t = t') 
+                                     | _                  -> 0 in
+                                   ((Value.of_int res)::st', config)
+    | ENTER xs                  -> let vs, st' = split (List.length xs) st in
+                                   (st', (State.push s (List.fold_left (fun s' (x, v) -> State.bind x v s') State.undefined (List.combine xs vs)) xs, i, o))
+    | LEAVE                     -> (st, (State.drop s, i, o))
     in match p with
-      | x::xs -> (match x with 
+       | x::xs -> ((*Printf.printf "Exec: %s\n" (show(insn) x);*) match x with 
         | JMP l -> eval env (controlSt, st, config) (env#labeled l)
         | CJMP (znz, l) -> (match st with
-                              | head::tail -> let v = (Value.to_int head) in 
+                            | head::tail -> let v = (Value.to_int head) in
                                               if (znz = "z" && v == 0) || (znz = "nz" && v <> 0) then eval env (controlSt, tail, config) (env#labeled l)
                                               else eval env (controlSt, tail, config) xs 
                               | _          -> failwith "CJMP failed with empty stack"
                            )
         | CALL (f, n, fl) -> if env#is_label f then eval env ((xs, s)::controlSt, st, config) (env#labeled f)
-                             else eval env (env#builtin (controlSt, st, config) f n (not fl)) xs
+                             else eval env (env#builtin (controlSt, st, config) f n fl) xs
         | RET _ | END     -> (match controlSt with
                                 | (p, oldS)::controlSt' -> let s' = State.leave s oldS in
                                                        eval env (controlSt', st, (s', i, o)) p
@@ -109,7 +121,7 @@ let run p i =
          method builtin (cstack, stack, (st, i, o)) f n p =
            let f = match f.[0] with 'L' -> String.sub f 1 (String.length f - 1) | _ -> f in
            let args, stack' = split n stack in
-           let (st, i, o, r) = Language.Builtin.eval (st, i, o, None) args f in
+           let (st, i, o, r) = Language.Builtin.eval (st, i, o, None) (List.rev args) f in
            let stack'' = if p then stack' else let Some r = r in r::stack' in
            (*Printf.printf "Builtin:\n";*)
            (cstack, stack'', (st, i, o))
@@ -130,20 +142,39 @@ let run p i =
 let labelGen = object 
    val mutable freeLabel = 0
    method get = freeLabel <- freeLabel + 1; "L" ^ string_of_int freeLabel
-end
+  end
+
+let rec matchPattern lFalse = function
+  | Stmt.Pattern.Wildcard | Stmt.Pattern.Ident _ -> false, [DROP]
+  | Stmt.Pattern.Sexp (t, ps) -> true, [DUP; TAG t; CJMP ("z", lFalse)] @ (
+                                   List.concat @@
+                                     List.mapi (fun i p -> [DUP; CONST i; CALL (".elem", 2, false)] @ snd (matchPattern lFalse p)) ps)
+
+let makeBindings p =
+  let rec inner = function
+    | Stmt.Pattern.Wildcard -> [DROP]
+    | Stmt.Pattern.Ident n  -> [SWAP]
+    | Stmt.Pattern.Sexp (_, ps) -> (List.flatten @@
+                                      List.mapi (fun i p -> [DUP; CONST i; CALL (".elem", 2, false)] @ inner p) ps
+                                   ) @ [DROP] in
+  inner p @ [ENTER (Stmt.Pattern.vars p)]
 
 let rec compileWithLabels p lastL =
-  let rec expr = function
+  let rec call f args fl =
+    let argsCode = List.flatten @@ List.map expr args in
+    argsCode @ [CALL (f, List.length args, fl)]
+  and expr = function
   | Expr.Var   x          -> [LD x]
   | Expr.Const n          -> [CONST n]
   | Expr.String s         -> [STRING s]
   | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
   | Expr.Array xs         -> let compiledXs = List.flatten (List.map (expr) (List.rev xs)) in
-                             compiledXs @ [CALL ("$array", (List.length compiledXs), true)]
-  | Expr.Elem (a, i)      -> expr i @ expr a @ [CALL ("$elem", 2, true)]
-  | Expr.Length n         -> expr n @ [CALL ("$length", 1, true)]
-  | Expr.Call (fName, argsE) -> let compiledArgs = List.flatten (List.map (expr) (List.rev argsE)) in
-                                compiledArgs @ [CALL (fName, List.length argsE, true)]
+                             compiledXs @ [CALL (".array", (List.length compiledXs), false)]
+  | Expr.Sexp (t, xs)     -> let compiledXs = List.flatten (List.map (expr) (List.rev xs)) in
+                             compiledXs @ [SEXP (t, List.length xs)]
+  | Expr.Elem (a, i)      -> expr i @ expr a @ [CALL (".elem", 2, false)]
+  | Expr.Length n         -> expr n @ [CALL (".length", 1, false)]
+  | Expr.Call (fName, argsE) -> call fName argsE false
   in match p with
   | Stmt.Seq (s1, s2)  -> (let newLabel = labelGen#get in
                            let (compiled1, used1) = compileWithLabels s1 newLabel in
@@ -165,12 +196,17 @@ let rec compileWithLabels p lastL =
   | Stmt.Repeat (body, e)  -> let lLoop = labelGen#get in
                               let (repeatBody, _) = compileWithLabels body lastL in
                               ([LABEL lLoop] @ repeatBody @ expr e @ [CJMP ("z", lLoop)]), false
-  | Stmt.Call (fName, argsE) -> let compiledArgs = List.flatten (List.map (expr) (List.rev argsE)) in
-                                compiledArgs @ [CALL (fName, List.length argsE, false)], false
+  | Stmt.Call (fName, argsE) -> call fName argsE true, false
   | Stmt.Return e            -> (match e with
                                    | Some x -> (expr x) @ [RET true]
                                    | None   -> [RET false]
                                 ), false
+  | Stmt.Leave               -> [LEAVE], false
+  | Stmt.Case (e, [p, s])    -> (* TODO: Reverse matchPattern return tuple *)
+                                let pUsed, pCode = matchPattern lastL p in
+                                let sBody, sUsed = compileWithLabels (Stmt.Seq (s, Stmt.Leave)) lastL in
+                                expr e @ pCode @ makeBindings p @ sBody, pUsed || sUsed
+
   | Stmt.Skip                -> [], false
 
 let compileP p =
